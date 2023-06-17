53bab037370faddee746e1f71b0bf1e04be4311e
Grant G <contact@iowegian.com>
2023-06-17 11:48:35 -0500

    Sorted statistics table Refactored and fixed components option Refactored and added table output Added command-line arguments

--- audio_stats.py
+++ audio_stats.py
@@ -1,10 +1,77 @@
+"""Calculates statistics for audio tone samples
+"""
+
 import math, sys, wave
 import numpy as np
 from scipy.fftpack import fft, rfft
 from scipy import signal
 import librosa as lr
+import argparse
+import glob
 
 class AudioStats:
+
+    min_db = -120.0
+
+    def __init__(self):
+        # File name
+        self.name = ''
+
+        # Time stats
+        self.len = 0
+        self.min = 0.0
+        self.max = 0.0
+        self.fs = 1.0
+        self.freq = 0.0
+
+        # Frequency stats
+        self.dc = self.min_db
+        self.snr = self.min_db
+        self.sfdr = self.min_db
+        self.sinad = self.min_db
+        self.thd = self.min_db
+        self.thd_pct = 100.0
+
+    def print_time(self):
+        print(f'Len   = {self.len}')
+        print(f'Min   = {self.min:6.3f}')
+        print(f'Max   = {self.max:6.3f}')
+        print(f'Fs    = {self.fs} Hz')
+        print(f'Freq  = {self.freq:.1f} Hz')
+        try:
+            print(f'Freq2 = {self.freq2:.1f} Hz')
+        except AttributeError:
+            pass
+
+    def print_freq(self):
+        print(f'SFDR  = {self.sfdr:3.1f} dB')
+        print(f'SNR   = {self.snr:3.1f} dB')
+        print(f'SINAD = {self.sinad:3.1f} dB')
+        print(f'DC    = {self.dc:3.1f} dB')
+        print(f'THD   = {self.thd:3.1f} dB')
+        print(f'THD%  = {self.thd_pct:.4f}%')
+
+    def print_header(self):
+        print('Name                                THD %   THD    SNR    SFDR')
+        print('---------------------------------------------------------------')
+
+    def print(self):
+        print(f'{self.name:35} {self.thd_pct:.4f} {self.thd:6.1f} {self.snr:6.1f} {self.sfdr:6.1f}')
+
+    def stats(self):
+        return {
+                'DC':self.dc,
+                'SNR':self.snr,
+                'SFDR':self.sfdr,
+                'SINAD':self.sinad,
+                'THD':self.thd,
+                'THD%':self.thd_pct
+               }
+    
+    def stats_index(self):
+        return [ 'DC', 'SNR', 'SFDR', 'SINAD', 'THD', 'THD%' ]
+    
+class AudioAnalyzer:
 
     def __init__(self, audio=None, fs=1.0, name=None):
         self.audio = audio
@@ -15,6 +82,8 @@ class AudioStats:
         return lr.power_to_db(pwr, amin=1e-12)
 
     def gen_test_audio(self, thd=0.01):
+        print("Generating test audio")
+
         # Generate a fundamental and a smaller harmonic
         fs = 44100
         freq = 1000
@@ -98,7 +167,7 @@ class AudioStats:
 
         # Compare the second-largest power with the first to
         # determine if this is a two-tone signal
-        if(self.comps[1][1] < 0.03*self.comps[0][1]):           # 0.03 ~= -15 dB
+        if(self.comps[1][1] < 0.001*self.comps[0][1]):      # 0.001 = 0.1% = -30 dB
             # second-largest is small: single tone signal
             start = 1
             dist = [n * self.freq for n in range(20)]
@@ -126,48 +195,51 @@ class AudioStats:
             else:
                 noise_pwr += pwr
 
-        # Calculate statistics from powers
-        self.dc = self.db(self.dc/self.sig_pwr)
-        self.snr = self.db(noise_pwr/self.sig_pwr)
-        self.sfdr = self.db(sfdr_pwr/self.sig_pwr)
-        self.sinad = self.db((noise_pwr + dist_pwr)/self.sig_pwr)
-        self.thd = self.db(dist_pwr/self.sig_pwr)
-        self.thd_pct = math.sqrt(dist_pwr/self.sig_pwr) * 100.0
+        stats = AudioStats()
+
+        stats.name = self.name
+        stats.len = len(self.audio)
+        stats.min = min(self.audio)
+        stats.max = max(self.audio)
+        stats.fs  = self.fs
+        stats.freq  = self.freq
+        try:
+            stats.freq2 = self.freq2
+        except AttributeError:
+            pass
+
+        # Calculate frequency statistics from powers
+        stats.dc = self.db(self.dc/self.sig_pwr)
+        stats.snr = self.db(noise_pwr/self.sig_pwr)
+        stats.sfdr = self.db(sfdr_pwr/self.sig_pwr)
+        stats.sinad = self.db((noise_pwr + dist_pwr)/self.sig_pwr)
+        stats.thd = self.db(dist_pwr/self.sig_pwr)
+        stats.thd_pct = math.sqrt(dist_pwr/self.sig_pwr) * 100.0
+
+        self.stats = stats
+        return stats
 
     def print_comps(self):
         # Print components in dB relative to signal power
-        print('Audio Components')
+        print('Components')
         print('-----------------------')        
         for (freq, pwr) in self.comps:
             freq = round(freq)
             db = self.db(pwr/self.sig_pwr)
             print(f'  {freq:5} Hz = {db:6.1f} dB')
 
-    def print_stats(self):
-        print('Audio Statistics')
-        print('-----------------------')
-        print(f'Samples = {len(self.audio)}')
-        print(f'Min     = {min(self.audio):6.3f}')
-        print(f'Max     = {max(self.audio):6.3f}')
-        print(f'Fs      = {self.fs} Hz')
-        print(f'Freq    = {self.freq:.1f} Hz')
-        try:
-            print(f'Freq2   = {self.freq2:.1f} Hz')
-        except AttributeError:
-            pass
-        print(f'SFDR    = {self.sfdr:3.1f} dB')
-        print(f'SNR     = {self.snr:3.1f} dB')
-        print(f'SINAD   = {self.sinad:3.1f} dB')
-        print(f'DC      = {self.dc:3.1f} dB')
-        print(f'THD     = {self.thd:3.1f} dB')
-        print(f'THD%    = {self.thd_pct:.4f}%')
-
-    def print(self):
+    def print(self, args):
         if self.name:
-            print( f'Name    = {self.name}\n')
-        self.print_comps()
-        print()
-        self.print_stats()
+            print( f'\n{self.name}')
+
+        if args.components:
+            self.print_comps()
+
+        if args.stats:
+            print('Statistics')
+            print('-----------------------')            
+            self.stats.print_time()
+            self.stats.print_freq()
 
     def stats(self):
         return {
@@ -175,25 +247,65 @@ class AudioStats:
                 'Min':min(self.audio),
                 'Max':max(self.audio),
                 'Fs':self.fs,
-                'Freq':self.freq,
-                'DC':self.dc,
-                'SNR':self.snr,
-                'SFDR':self.sfdr,
-                'SINAD':self.sinad,
-                'THD':self.thd,
-                'THD%':self.thd_pct
+                'Freq':self.freq
                }
     
     def stats_index(self):
-        return [ 'Samples', 'Min', 'Max', 'Fs', 'Freq', 'DC', 'SNR', 'SFDR', 'SINAD', 'THD', 'THD%' ]
+        return [ 'Samples', 'Min', 'Max', 'Fs', 'Freq' ]
+
+def main():
+    # Create argument parser
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument(
+        "pattern", metavar='PATTERN',
+        help="file name pattern", nargs='?', default=None
+    )
+    parser.add_argument(
+        '-c', '--components', action='store_true', default=False,
+        help="frequency components"
+    ) 
+    parser.add_argument(
+        '-g', '--generate', action='store_true',
+        help="generate test audio", default=None
+    )
+    parser.add_argument(
+        '-s', '--stats', action='store_true', default=False,
+        help="statistics"
+    )
+    parser.add_argument(
+        '-t', '--table', action='store_true', default=False,
+        help="statistics table"
+    )    
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Create an audio analyzer object
+    a = AudioAnalyzer()
+
+    # Generate if specified
+    if args.generate:
+        a.gen_test_audio()
+        a.analyze()
+        a.print(args)
+    
+    # Load, analyze, and print input files if specified    
+    if args.pattern:
+        stats = []
+        files = glob.glob(args.pattern)
+        for f in files:
+            a.load(f)
+            stats.append(a.analyze())
+            a.print(args)
+
+        if stats and args.table:
+            AudioStats().print_header()
+        stats.sort(key=lambda st: st.thd)
+        for stat in stats:
+            stat.print()
 
 if __name__ == "__main__":
-    a = AudioStats()
-    if len(sys.argv) > 1:
-        a.load(sys.argv[1])
-    else:
-        print("Generating test audio")
-        a.gen_test_audio()
-    a.analyze()
-
-    a.print()
+    main()
